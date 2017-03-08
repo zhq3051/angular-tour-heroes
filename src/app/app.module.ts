de49e1f03192eb842ee523a6ef1acb8f26ddc41a
zeke <zhengq@shinetechchina.com>
2017-03-07 18:47:54 -0800

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { AppComponent } from './app.component';
+import { HeroService } from './hero.service';
+import { AppRoutingModule} from './app-routing.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap: [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
+
 export class AppModule { }
